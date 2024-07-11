f3fe376b7c598091a3dece3821b422fda66ded5f
Alexis Espinosa <Alexis.Espinosa@pawsey.org.au>
2024-07-11 16:36:25 +0800

    Modifying to compile everything with hipcc

--- setEnv.sh
+++ setEnv.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+# Load the modules for the dependencies
 module use /software/projects/pawsey1016/setonix/2024.05/modules/zen3/gcc/12.2.0/
 module load intel-tbb/2021.9.0-popklgx
 module load boost/1.83.0-3nhulww
 module load .zlib/1.3-l3er4yi
-module load craype-accel-amd-gfx90a
-module load cmake/3.27.7
-module load rocm/5.6.1
 
+# Load the modules for compiling the code
+module unload cray-libsci
+module load craype-accel-amd-gfx90a
+module load rocm/5.6.1
+module load cmake/3.27.7
+
+# Improving the inclusion paths
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${ROCM_PATH}/include:${ROCM_PATH}/hip/include:${ROCM_PATH}/llvm/include
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${ROCM_PATH}/lib:${ROCM_PATH}/hip/lib:${ROCM_PATH}/llvm/lib
+export LIBRARY_PATH=$LIBRARY_PATH:${ROCM_PATH}/lib:${ROCM_PATH}/hip/lib:${ROCM_PATH}/llvm/lib
+
+# Extra flags to enable GPU support
 export HIP_PLATFORM=amd
+export GPU_ARCH="gfx90a"
+MPICH_GPU_SUPPORT_ENABLED=1
+
+# Any extra MPI libs here
+export MPI_EXTRA_LIBS="-L${CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_hsa"
+
+# Using the "raw" hipcc compiler
+unset CC CXX
+export CC=${ROCM_PATH}/bin/hipcc
+export CXX=${ROCM_PATH}/bin/hipcc
+
